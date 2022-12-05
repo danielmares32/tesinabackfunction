24589a930ce13f6336b2bfb5209a1a610e5696c6
Daniel Mares <danielmares32@gmail.com>
2022-12-05 13:10:26 -0600

    cambio

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -3,9 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 export async function createApp(): Promise<INestApplication> {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();  
+  const app = await NestFactory.create(AppModule); 
+  app.enableCors();
+  app.setGlobalPrefix('api'); 
   await app.init();
   return app;
 }
